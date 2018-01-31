cbdb67e45af0bf5d386cc570e52c9218950038b3
Raul Barboza <ruzito@gmail.com>
2018-01-31 19:08:01 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {deadline: 'December 25, 2017'}
+        
+    }
+    render() {
+        return(
+        <div className="App">
+            <div className="App-title">Countdown to {this.state.deadline}</div>
+            <div>
+                <div className="Clock-days">14 days</div>
+                <div className="Clock-hours">30 hours</div>
+                <div className="Clock-minutes">15 minutes</div>
+                <div className="Clock-seconds">20 seconds</div>
+            </div>
+            <input placeholder='new date'/>
+            <button>Submit</button>
+        </div>
+            )
+
+    }
+}
+
+export default App;
